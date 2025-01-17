c141c88247244a3d425f6bdff782563bbf35e667
Blackman <DBlackman@int.co.chenango.ny.us>
2025-01-17 11:42:28 -0500

    initial firebase

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyDC4ZiyNiG8g-JeTsU61W0LhzXHy5FmJ5E",
+authDomain: "leads-tracker-app-8f09d.firebaseapp.com",
+databaseURL: "https://leads-tracker-app-8f09d-default-rtdb.firebaseio.com",
+projectId: "leads-tracker-app-8f09d",
+storageBucket: "leads-tracker-app-8f09d.firebasestorage.app",
+messagingSenderId: "175869777579",
+appId: "1:175869777579:web:c6927dbe7ef196e05489ee",
+measurementId: "G-8GFLQM4JDJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "myLeads")
+
+console.log(database)
 
 
 const inputBtn = document.getElementById("input-btn")
-const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const input = document.getElementById("input")
 const list = document.getElementById("leads-list")
-const storedLeads =  JSON.parse(localStorage.getItem("myLeads"))
 
-let myLeads = []
-
-// Page load render
-if (storedLeads) {
-    myLeads = storedLeads
-    render(myLeads)
-}
-
+onValue(referenceInDB, (snapshot) => {
+    console.log(snapshot.val())
+})
 // Render Function
 
 function render(leads) {
@@ -25,31 +45,17 @@ function render(leads) {
     list.innerHTML = listItems
 }
 
-// Save tab button 
-
-tabBtn.addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
-
 // Save input button
 
 inputBtn.addEventListener("click", () => {
-    console.log("clicked")
-    myLeads.push(`https://${input.value}`)
-    input.textContent = ''
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    push(referenceInDB, input.value)
+    input.value = ''
+    
+    //render(leads)
 })
 
 // Delete button
 
 deleteBtn.addEventListener("click", () => {
-    input.textContent = ''
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    input.value = ''
 })
